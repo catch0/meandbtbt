04fa1229a0c57dcd78df10509a034fb0076a7c7c
Spence Peacock <spencerpeacock@gmail.com>
2017-08-25 15:48:34 -0500

    changed server port from 4000 to 8000

--- server.js
+++ server.js
@@ -19,6 +19,6 @@ require('./server/config/mongoose');
 
 require('./server/config/routes')(app);
 
-app.listen(4000, function(){
-  console.log('listening on port 4000...')
+app.listen(8000, function(){
+  console.log('listening on port 8000...')
 })
