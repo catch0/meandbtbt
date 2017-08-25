7b35fc746af2e46b23a7bd8de0b93278974c5cbd
Spence Peacock <spencerpeacock@gmail.com>
2017-08-25 13:05:11 -0500

    styling

--- public/src/app/landing/landing.component.ts
+++ public/src/app/landing/landing.component.ts
@@ -19,6 +19,7 @@ export class LandingComponent implements OnInit {
   ) { }
 
   ngOnInit() {
+    this.getArticles();
   }
 getArticles(){
   return this._articleService.getArticles()
--- server/models/article.js
+++ server/models/article.js
@@ -5,12 +5,11 @@ let ArticleSchema = new mongoose.Schema({
     type: String,
     required: [true, 'please submit a question'],
     minlength: [6, 'question needs to be at least 6 charachters '],
-    maxlength:[140, 'question cannot be more than 140 charachters']
   },
   url:{
     type: String,
   },
-  tredingWord:{
+  trendingWord:{
     type: String,
   },
   score:{
