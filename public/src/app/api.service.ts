c25e156acd01aeab868d21c4f698d52458f9570d
Spence Peacock <spencerpeacock@gmail.com>
2017-08-27 14:20:28 -0500

    wrote function for api service

--- public/src/app/api.service.ts
+++ public/src/app/api.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@angular/core';
+import {Http} from '@angular/http';
+import 'rxjs';
+
 
 @Injectable()
 export class ApiService {
 
-  constructor() { }
+  constructor(private _http:Http) { }
+  getArticles(search){
+    console.log('getting article')
+      return this._http.get(`http://api.github.com/aritcles/${search}`).map(data=>data.json()).toPromise()
+  }
 
 }
