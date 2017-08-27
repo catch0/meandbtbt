5e364f11a16549f4f10d09cf1f8d8c59730b8fa5
Spence Peacock <spencerpeacock@gmail.com>
2017-08-27 09:41:14 -0500

    created api service

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -3,7 +3,17 @@ let fs = require('fs');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost/debatebot', { useMongoClient: true })
+mongoose.connect('mongodb://dbot:Inexile!2@news-prototype-shard-00-00-pjiq3.mongodb.net:27017,news-prototype-shard-00-01-pjiq3.mongodb.net:27017,news-prototype-shard-00-02-pjiq3.mongodb.net:27017/test?ssl=true&replicaSet=News-Prototype-shard-0&authSource=admin', { useMongoClient: true })
+var MongoClient = require('mongodb').MongoClient;
+
+
+// var uri = "mongodb://kay:Inexile!2@mycluster0-shard-00-00-wpeiv.mongodb.net:27017,mycluster0-shard-00-01-wpeiv.mongodb.net:27017,mycluster0-shard-00-02-wpeiv.mongodb.net:27017/admin?ssl=true&replicaSet=Mycluster0-shard-0&authSource=admin";
+// MongoClient.connect(uri, function(err, db) {
+//   db.close();
+// });
+
+
+
 
 let models_path = __dirname + '/../models';
 
